eb67b6c7e43ee1685c11e0f6f20c91267f629917
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-06-25 09:56:12 +0300

    fixed bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Titleh1 } from "./pages.styled";
 export default function Home() {
     return (
         <Titleh1>
-            My phonenook
+            Phonebook
         </Titleh1>
     );
 }
